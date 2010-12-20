fbd014c7fda8591ffacd0c5f932cbf28b7daf40f
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2010-12-20 04:37:36 +0100

    Fixed private settings file reading.

--- psycobuild/pcfg.py
+++ psycobuild/pcfg.py
@@ -4,10 +4,15 @@ import os
 
 from twisted.python import log
 
+builddir = os.path.dirname(__file__)
+fn = os.path.abspath(builddir + "/../private_settings.py")
+
 pcfg = {}
-if os.path.exists('./private_settings.py'):
-    log.msg("reading private settings file")
-    execfile('./private_settings.py', pcfg)
+if os.path.exists(fn):
+    log.msg("reading private settings file %s", fn)
+    execfile(fn, pcfg)
+else:
+    log.msg("private settings file not found: %s", fn)
 
 
 def get(key, default=None):
